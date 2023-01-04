2225fb5a62d441418be899f47d6dd995567c14de
WuX <axel.riska@gmail.com>
2023-01-04 07:54:06 +0100

    Added more implementation to libc. Also, realized glibc might have to be implemented

--- libc/src/string.c
+++ libc/src/string.c
@@ -1,16 +1,36 @@
 #include "string.h"
 
 size_t strlen(const char *str) {
-    size_t counter = 0;
-    while(str[counter] != 0) ++counter;
-    return counter;
+    size_t i = 0;
+    while(str[i]) ++i;
+    return i;
 }
 
 int strcmp(const char *lhs, const char *rhs) {
-    for(size_t i = 0; ; i++) {
-        if(lhs[i] != rhs[i])
-            return lhs[i] < rhs[i] ? -1 : 1;
-        if(!lhs[i])
-            return 0;
+    size_t i = 0;
+    while(lhs[i] == rhs[i])
+        if(!lhs[i++]) return 0;
+    return lhs[i] < rhs[i] ? -1 : 1;
+}
+
+void *memset(void *dest, int ch, size_t count) {
+    char *temp = (char *) dest;
+    for(; count > 0; count--) *temp++ = (unsigned char) ch;
+    return dest;
+}
+
+void *memcpy(void *dest, const void *src, size_t count) {
+    for(size_t i = 0; i < count; i++)
+        *((unsigned char *) dest + i) = *((unsigned char *) src + i);
+    return dest;
+}
+
+int memcmp(const void *lhs, const void *rhs, size_t count) {
+    for(size_t i = 0; i < count; i++) {
+        if(*((unsigned char *) lhs + i) > *((unsigned char *) rhs + i))
+            return -1;
+        if(*((unsigned char *) lhs + i) < *((unsigned char *) rhs + i))
+            return 1;
     }
+    return 0;
 }
