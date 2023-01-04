86b3f05302922d0a9f39f571b0c478ba2d68d5ef
WuX <wux@thenest.dev>
2023-01-04 04:28:44 +0100

    Rudamentary virtual paging, also fixed physical paging

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -17,16 +17,19 @@ int putchar(int c) {
     switch(c) {
         case '\n':
             g_x = 0;
-            g_y += BASIC_FONT_HEIGHT;
+            g_y += BASICFONT_HEIGHT;
+            break;
+        case '\t':
+            g_x += (4 - (g_x / BASICFONT_WIDTH) % 4) * BASICFONT_WIDTH;
             break;
         default:
-            uint8_t *font_char = &g_basic_font[c * 16];
+            uint8_t *font_char = &g_basicfont[c * 16];
 
-            uint32_t *buf = (uint32_t *) (uint64_t) g_framebuffer.address;
+            uint32_t *buf = (uint32_t *) HHDM(g_framebuffer.address);
             int offset = g_x + g_y * g_framebuffer.pitch / 4;
-            for(int xx = 0; xx < BASIC_FONT_HEIGHT; xx++) {
-                for(int yy = 0; yy < BASIC_FONT_WIDTH; yy++) {
-                    if(font_char[xx] & (1 << (BASIC_FONT_WIDTH - yy))){
+            for(int xx = 0; xx < BASICFONT_HEIGHT; xx++) {
+                for(int yy = 0; yy < BASICFONT_WIDTH; yy++) {
+                    if(font_char[xx] & (1 << (BASICFONT_WIDTH - yy))){
                         buf[offset + yy] = 0xFFFFFFFF;
                     } else {
                         buf[offset + yy] = 0;
@@ -34,14 +37,14 @@ int putchar(int c) {
                 }
                 offset += g_framebuffer.pitch / 4;
             }
-            g_x += BASIC_FONT_WIDTH;
+            g_x += BASICFONT_WIDTH;
             break;
     }
     if(g_x >= g_framebuffer.width) {
         g_x = 0;
-        g_y += BASIC_FONT_HEIGHT;
+        g_y += BASICFONT_HEIGHT;
     }
-    if(g_y >= g_framebuffer.height) g_y = g_framebuffer.height - BASIC_FONT_HEIGHT - 1;
+    if(g_y >= g_framebuffer.height) g_y = g_framebuffer.height - BASICFONT_HEIGHT - 1;
     return c;
 }
 
@@ -52,12 +55,15 @@ extern noreturn void kmain(tartarus_parameters_t *boot_params) {
     printf("Welcome to Elysium OS\n");
 
     pmm_initialize(boot_params->memory_map, boot_params->memory_map_length);
-
     printf("Physical Memory Initialized\n");
+    printf("Stats:\n\tTotal: %x\n\tFree: %x\n\tUsed: %x\n", pmm_mem_total(), pmm_mem_free(), pmm_mem_used());
+
+    printf("HHDM is at %x\n", g_hhdm_address);
 
     uint64_t pml4;
     asm volatile("mov %%cr3, %0" : "=r" (pml4));
     vmm_initialize(pml4);
+    printf("Virtual Memory Initialized\n");
 
     while(true) asm("hlt");
 }
---
+++ kernel/src/tmplibc.c
@@ -0,0 +1,19 @@
+#include "tmplibc.h"
+
+void memcpy(void *src, void *dest, int nbytes) {
+    for(int i = 0; i < nbytes; i++)
+        *((uint8_t *) dest + i) = *((uint8_t *) src + i);
+}
+
+void memset(uint8_t value, void *dest, int len) {
+    uint8_t *temp = (uint8_t *) dest;
+    for(; len > 0; len--) *temp++ = value;
+}
+
+bool memcmp(void *cmp1, void *cmp2, int nbytes) {
+    for(int i = 0; i < nbytes; i++) {
+        if(*((uint8_t *) cmp1 + i) != *((uint8_t *) cmp2 + i))
+            return false;
+    }
+    return true;
+}
