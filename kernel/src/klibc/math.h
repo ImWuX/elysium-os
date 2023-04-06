dcb0571997e16afe600c506c1e66f8e2db72d3d0
WuX <axel.riska@gmail.com>
2023-04-06 13:30:34 +0200

    New printf implementation. Considerably more comprehensive, it includes everything but %n, %p, and all SSE ops

--- kernel/src/klibc/math.h
+++ kernel/src/klibc/math.h
@@ -2,5 +2,7 @@
 #define LIBC_MATH_H
 
 int abs(int n);
+int max(int a, int b);
+int min(int a, int b);
 
 #endif
--- kernel/src/klibc/stdio.h
+++ kernel/src/klibc/stdio.h
@@ -2,11 +2,13 @@
 #define LIBC_STDIO_H
 
 #include <stdint.h>
+#include <stdarg.h>
 
 extern int putchar(int character);
 
 typedef unsigned long size_t;
 
-void printf(const char *fmt, ...);
+int printf(const char *fmt, ...);
+int vprintf(const char *format, va_list list);
 
 #endif
