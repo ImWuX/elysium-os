557df35fb661723f04321fd1bc349119a1eaed02
WuX <axel.riska@gmail.com>
2023-06-29 07:47:46 +0200

    AMD64 :: Introduced support for the APIC and interrupts

---
+++ kernel/src/arch/amd64/interrupt.h
@@ -0,0 +1,76 @@
+#ifndef CPU_INTERRUPT_H
+#define CPU_INTERRUPT_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct {
+    uint64_t r15;
+    uint64_t r14;
+    uint64_t r13;
+    uint64_t r12;
+    uint64_t r11;
+    uint64_t r10;
+    uint64_t r9;
+    uint64_t r8;
+    uint64_t rdi;
+    uint64_t rsi;
+    uint64_t rbp;
+    uint64_t rdx;
+    uint64_t rcx;
+    uint64_t rbx;
+    uint64_t rax;
+    uint64_t int_no;
+    uint64_t err_code;
+    uint64_t rip;
+    uint64_t cs;
+    uint64_t rflags;
+    uint64_t rsp;
+    uint64_t ss;
+} interrupt_frame_t;
+
+typedef enum {
+    INTERRUPT_PRIORITY_EXCEPTION = 0,
+    INTERRUPT_PRIORITY_DISK = 10,
+    INTERRUPT_PRIORITY_HID = 13,
+    INTERRUPT_PRIORITY_TIMER = 14,
+    INTERRUPT_PRIORITY_KERNHIGH = 15
+} interrupt_priority_t;
+
+typedef void (* interrupt_handler_t)(interrupt_frame_t *frame);
+typedef void (* interrupt_irq_eoi_t)(uint8_t);
+
+extern interrupt_irq_eoi_t g_interrupt_irq_eoi;
+
+/**
+ * @brief Initialize the IDT and interrupt management
+ */
+void interrupt_initialize();
+
+/**
+ * @brief Set a handler onto an interrupt vector
+ * @warning Will carelessly override existing handlers
+ *
+ * @param vector Interrupt vector
+ * @param priority Interrupt priority
+ * @param handler Interrupt handler
+ */
+void interrupt_set(uint8_t vector, interrupt_priority_t priority, interrupt_handler_t handler);
+
+/**
+ * @brief Request a free interrupt vector
+ *
+ * @param priority Interrupt priority
+ * @param handler Interrupt handler
+ * @return Chosen interrupt vector or -1 on error
+ */
+int interrupt_request(interrupt_priority_t priority, interrupt_handler_t handler);
+
+/**
+ * @brief Signal an end of interrupt
+ *
+ * @param vector Interrupt vector
+ */
+void interrupt_irq_eoi(uint8_t vector);
+
+#endif
--- kernel/src/panic.c
+++ kernel/src/panic.c
@@ -1,6 +1,80 @@
 #include "panic.h"
+#include <stdio.h>
+
+typedef struct stack_frame {
+    struct stack_frame *rbp;
+    uint64_t rip;
+} __attribute__((packed)) stack_frame_t;
+
+static char *g_exception_messages[] = {
+    "Division by Zero",
+    "Debug",
+    "Non-Maskable Interrupt",
+    "Breakpoint",
+    "Overflow",
+    "Out of Bounds",
+    "Invalid Opcode",
+    "No Coprocessor",
+    "Double Fault",
+    "Coprocessor Segment Overrun",
+    "Bat TSS",
+    "Segment not Present",
+    "Stack Fault",
+    "General Protection Fault",
+    "Page Fault",
+    "Unknown Interrupt",
+    "Coprocessor Fault",
+    "Alignment Check",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved"
+};
 
 [[noreturn]] void panic(char *location, char *msg) {
-    asm volatile("cli\nhlt");
+    printf("KERNEL PANIC\n[%s] %s\n", location, msg);
+    asm volatile("cli");
+    while(true) asm volatile("hlt");
+    __builtin_unreachable();
+}
+
+[[noreturn]] void panic_exception(interrupt_frame_t *frame) {
+    uint64_t cr2_value;
+    asm volatile("movq %%cr2, %0" : "=r" (cr2_value));
+    printf("UNHANDLED EXCEPTION\n%s\n", g_exception_messages[frame->int_no]);
+    printf("r15: %#lx\n", frame->r15);
+    printf("r14: %#lx\n", frame->r14);
+    printf("r13: %#lx\n", frame->r13);
+    printf("r12: %#lx\n", frame->r12);
+    printf("r11: %#lx\n", frame->r11);
+    printf("r10: %#lx\n", frame->r10);
+    printf("r9: %#lx\n", frame->r9);
+    printf("r8: %#lx\n", frame->r8);
+    printf("rdi: %#lx\n", frame->rdi);
+    printf("rsi: %#lx\n", frame->rsi);
+    printf("rbp: %#lx\n", frame->rbp);
+    printf("rdx: %#lx\n", frame->rdx);
+    printf("rcx: %#lx\n", frame->rcx);
+    printf("rbx: %#lx\n", frame->rbx);
+    printf("rax: %#lx\n", frame->rax);
+    printf("int_no: %#lx\n", frame->int_no);
+    printf("err_code: %#lx\n", frame->err_code);
+    printf("cr2: %#lx\n", cr2_value);
+    printf("rip: %#lx\n", frame->rip);
+    printf("cs: %#lx\n", frame->cs);
+    printf("rflags: %#lx\n", frame->rflags);
+    printf("rsp: %#lx\n", frame->rsp);
+    printf("ss: %#lx\n", frame->ss);
+    asm volatile("cli");
+    while(true) asm volatile("hlt");
     __builtin_unreachable();
 }
