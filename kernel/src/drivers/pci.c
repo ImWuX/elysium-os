c890bff88bc0f5365f4b8ffd6c89e81d86f81bea
WuX <wux@thenest.dev>
2023-07-05 13:13:52 +0200

    PCI :: Ported implementation from 2.0

---
+++ kernel/src/drivers/pci.c
@@ -0,0 +1,171 @@
+#include "pci.h"
+#include <panic.h>
+#include <drivers/acpi.h>
+#include <memory/hhdm.h>
+#include <memory/heap.h>
+
+#ifdef __ARCH_AMD64
+#include <arch/amd64/port.h>
+
+#define PORT_CONFIG_ADDRESS 0xCF8
+#define PORT_CONFIG_DATA 0xCFC
+#endif
+
+#define VENDOR_INVALID 0xFFFF
+#define CMD_REG_BUSMASTER (1 << 2)
+#define HEADER_TYPE_MULTIFUNC (1 << 7)
+
+typedef struct {
+    uint64_t base_address;
+    uint16_t segment_group_number;
+    uint8_t start_bus_number;
+    uint8_t end_bus_number;
+    uint32_t rsv0;
+} __attribute__((packed)) pcie_segment_entry_t;
+
+static pcie_segment_entry_t *g_segments;
+pci_device_t *g_pci_devices;
+
+static uint32_t readd(uint16_t segment, uint8_t bus, uint8_t device, uint8_t func, uint8_t offset) {
+    if(g_segments) bus -= g_segments[segment].start_bus_number;
+    uint32_t reg = ((uint32_t) bus << 16) | ((uint32_t) (device & 0x1F) << 11) | ((uint32_t) (func & 0x7) << 8);
+    offset &= 0xFC;
+    if(g_segments) {
+        return *((uint32_t *) (HHDM(g_segments[segment].base_address) + ((reg << 4) | offset)));
+    } else {
+#ifdef __ARCH_AMD64
+        port_outl(PORT_CONFIG_ADDRESS, reg | offset | (1 << 31));
+        return port_inl(PORT_CONFIG_DATA);
+#else
+        panic("PCI", "Legacy PCI unavailable");
+#endif
+    }
+}
+
+static uint16_t readw(uint16_t segment, uint8_t bus, uint8_t device, uint8_t func, uint8_t offset) {
+    return (uint16_t) (readd(segment, bus, device, func, offset) >> ((offset & 2) * 8));
+}
+
+static uint8_t readb(uint16_t segment, uint8_t bus, uint8_t device, uint8_t func, uint8_t offset) {
+    return (uint8_t) (readd(segment, bus, device, func, offset) >> ((offset & 3) * 8));
+}
+
+static void writed(uint16_t segment, uint8_t bus, uint8_t device, uint8_t func, uint8_t offset, uint32_t value) {
+    if(g_segments) bus -= g_segments[segment].start_bus_number;
+    uint32_t reg = ((uint32_t) bus << 16) | ((uint32_t) (device & 0x1F) << 11) | ((uint32_t) (func & 0x7) << 8);
+    offset &= 0xFC;
+    if(g_segments) {
+        *((uint32_t *) (HHDM(g_segments[segment].base_address) + ((reg << 4) | offset))) = value;
+    } else {
+#ifdef __ARCH_AMD64
+        port_outl(PORT_CONFIG_ADDRESS, reg | offset | (1 << 31));
+        port_outl(PORT_CONFIG_DATA, value);
+#else
+        panic("PCI", "Legacy PCI unavailable");
+#endif
+    }
+}
+
+static void writew(uint16_t segment, uint8_t bus, uint8_t device, uint8_t func, uint8_t offset, uint16_t value) {
+    uint32_t ovalue = readd(segment, bus, device, func, offset);
+    ovalue |= 0xFFFF >> ~((offset & 2) * 8);
+    ovalue |= value >> ((offset & 2) * 8);
+    writed(segment, bus, device, func, offset, ovalue);
+}
+
+static void writeb(uint16_t segment, uint8_t bus, uint8_t device, uint8_t func, uint8_t offset, uint8_t value) {
+    uint32_t ovalue = readd(segment, bus, device, func, offset);
+    ovalue |= 0xFF >> ~((offset & 3) * 8);
+    ovalue |= value >> ((offset & 3) * 8);
+    writed(segment, bus, device, func, offset, ovalue);
+}
+
+static void check_bus(uint16_t segment, uint8_t bus);
+static void check_function(uint16_t segment, uint8_t bus, uint8_t device, uint8_t func) {
+    uint16_t vendor_id = readw(segment, bus, device, func, __builtin_offsetof(pci_device_header_t, vendor_id));
+    if(vendor_id == VENDOR_INVALID) return;
+
+    pci_device_t *pci_device = heap_alloc(sizeof(pci_device_t));
+    pci_device->segment = segment;
+    pci_device->bus = bus;
+    pci_device->slot = device;
+    pci_device->func = func;
+    pci_device->list = g_pci_devices;
+    g_pci_devices = pci_device;
+
+    uint8_t class = readb(segment, bus, device, func, __builtin_offsetof(pci_device_header_t, class));
+    uint8_t sub_class = readb(segment, bus, device, func, __builtin_offsetof(pci_device_header_t, sub_class));
+    uint8_t prog_if = readb(segment, bus, device, func, __builtin_offsetof(pci_device_header_t, program_interface));
+    switch(class) {
+        case 0x1:
+            switch(sub_class) {
+                case 0x6:
+                    switch(prog_if) {
+                        case 0x1:
+                            // ahci_initialize_device(pci_device);
+                            break;
+                    }
+                    break;
+            }
+            break;
+        case 0x6:
+            switch(sub_class) {
+                case 0x4:
+                    check_bus(segment, (uint8_t) (readb(segment, bus, device, func, __builtin_offsetof(pci_header1_t, secondary_bus)) >> 8));
+                    break;
+            }
+            break;
+    }
+}
+
+static void check_bus(uint16_t segment, uint8_t bus) {
+    for(uint8_t device = 0; device < 32; device++) {
+        if(readw(segment, bus, device, 0, __builtin_offsetof(pci_device_header_t, vendor_id)) == VENDOR_INVALID) continue;
+        for(uint8_t func = 0; func < ((readb(segment, bus, device, 0, __builtin_offsetof(pci_device_header_t, header_type)) & HEADER_TYPE_MULTIFUNC) ? 8 : 1); func++) check_function(segment, bus, device, func);
+    }
+}
+
+static void check_segment(uint16_t segment) {
+    if(readb(segment, 0, 0, 0, __builtin_offsetof(pci_device_header_t, header_type)) & HEADER_TYPE_MULTIFUNC) {
+        for(uint8_t func = 0; func < 8; func++) {
+            if(readw(segment, 0, 0, func, __builtin_offsetof(pci_device_header_t, vendor_id)) == VENDOR_INVALID) break;
+            check_bus(segment, func);
+        }
+    } else {
+        check_bus(segment, 0);
+    }
+}
+
+uint8_t pci_config_read_byte(pci_device_t *device, uint8_t offset) {
+    return readb(device->segment, device->bus, device->slot, device->func, offset);
+}
+
+uint16_t pci_config_read_word(pci_device_t *device, uint8_t offset) {
+    return readw(device->segment, device->bus, device->slot, device->func, offset);
+}
+
+uint32_t pci_config_read_double(pci_device_t *device, uint8_t offset) {
+    return readd(device->segment, device->bus, device->slot, device->func, offset);
+}
+
+void pci_config_write_byte(pci_device_t *device, uint8_t offset, uint8_t data) {
+    return writeb(device->segment, device->bus, device->slot, device->func, offset, data);
+}
+
+void pci_config_write_word(pci_device_t *device, uint8_t offset, uint16_t data) {
+    return writew(device->segment, device->bus, device->slot, device->func, offset, data);
+}
+
+void pci_config_write_double(pci_device_t *device, uint8_t offset, uint32_t data) {
+    return writed(device->segment, device->bus, device->slot, device->func, offset, data);
+}
+
+void pci_enumerate(acpi_sdt_header_t *mcfg) {
+    if(mcfg) g_segments = (pcie_segment_entry_t *) ((uintptr_t) mcfg + sizeof(acpi_sdt_header_t) + 8);
+#ifndef __ARCH_AMD64
+    else panic("PCI", "Legacy PCI unavailable");
+#endif
+    unsigned int entry_count = 1;
+    if(g_segments) entry_count = (mcfg->length - (sizeof(acpi_sdt_header_t) + 8)) / sizeof(pcie_segment_entry_t);
+    for(unsigned int i = 0; i < entry_count; i++) check_segment(i);
+}
---
+++ kernel/src/drivers/pci.h
@@ -0,0 +1,95 @@
+#ifndef DRIVERS_PCI_H
+#define DRIVERS_PCI_H
+
+#include <stdint.h>
+#include <drivers/acpi.h>
+
+typedef struct {
+    uint16_t vendor_id;
+    uint16_t device_id;
+    uint16_t command;
+    uint16_t status;
+    uint8_t revision_id;
+    uint8_t program_interface;
+    uint8_t sub_class;
+    uint8_t class;
+    uint8_t cache_line_size;
+    uint8_t latency_timer;
+    uint8_t header_type;
+    uint8_t bist;
+} __attribute__((packed)) pci_device_header_t;
+
+typedef struct {
+    pci_device_header_t device_header;
+
+    uint32_t bar0;
+    uint32_t bar1;
+    uint32_t bar2;
+    uint32_t bar3;
+    uint32_t bar4;
+    uint32_t bar5;
+
+    uint32_t cardbus_cis_pointer;
+    uint16_t subsystem_vendor_id;
+    uint16_t subsystem_id;
+    uint32_t expansion_rom_base_address;
+    uint8_t capabilities_pointer;
+    uint8_t rsv0;
+    uint16_t rsv1;
+    uint32_t rsv2;
+    uint8_t interrupt_line;
+    uint8_t interrupt_pin;
+    uint8_t min_grant;
+    uint8_t max_latency;
+} __attribute__((packed)) pci_header0_t;
+
+typedef struct {
+    pci_device_header_t device_header;
+
+    uint32_t bar0;
+    uint32_t bar1;
+
+    uint8_t primary_bus;
+    uint8_t secondary_bus;
+    uint8_t subordinate_bus;
+    uint8_t secondary_latency_timer;
+    uint8_t io_base_lower;
+    uint8_t io_limit_lower;
+    uint16_t secondary_status;
+    uint16_t memory_base_lower;
+    uint16_t memory_limit_lower;
+    uint16_t prefetchable_memory_base;
+    uint16_t prefetchable_memory_limit;
+    uint32_t prefetchable_memory_base_upper;
+    uint32_t prefetchable_memory_limit_upper;
+    uint16_t io_base_upper;
+    uint16_t io_limit_upper;
+    uint8_t capability_pointer;
+    uint8_t rsv0;
+    uint16_t rsv1;
+    uint32_t expansion_rom_base;
+    uint8_t interrupt_line;
+    uint8_t interrupt_pin;
+    uint16_t bridge_control;
+} __attribute__((packed)) pci_header1_t;
+
+typedef struct pci_device {
+    struct pci_device *list;
+    uint16_t segment;
+    uint8_t bus;
+    uint8_t slot;
+    uint8_t func;
+} pci_device_t;
+
+extern pci_device_t *g_pci_devices;
+
+void pci_enumerate(acpi_sdt_header_t *mcfg);
+
+uint8_t pci_config_read_byte(pci_device_t *device, uint8_t offset);
+uint16_t pci_config_read_word(pci_device_t *device, uint8_t offset);
+uint32_t pci_config_read_double(pci_device_t *device, uint8_t offset);
+void pci_config_write_byte(pci_device_t *device, uint8_t offset, uint8_t data);
+void pci_config_write_word(pci_device_t *device, uint8_t offset, uint16_t data);
+void pci_config_write_double(pci_device_t *device, uint8_t offset, uint32_t data);
+
+#endif
