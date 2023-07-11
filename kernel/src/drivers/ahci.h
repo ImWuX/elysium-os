dad79adadd9d2433e6b4190b2c59fdcadbc6c6e8
WuX <wux@thenest.dev>
2023-07-11 12:47:38 +0200

    AHCI :: Function descriptions

--- kernel/src/drivers/ahci.h
+++ kernel/src/drivers/ahci.h
@@ -4,7 +4,21 @@
 #include <stdint.h>
 #include <drivers/pci.h>
 
+/**
+ * @brief Initialize AHCI driver for a device.
+ *
+ * @param device PCI device
+ */
 void ahci_initialize_device(pci_device_t *device);
+
+/**
+ * @brief Read sectors from disk.
+ * 
+ * @param port Port
+ * @param sector LBA
+ * @param sector_count Sector count
+ * @param dest Destination
+ */
 void ahci_read(uint8_t port, uint64_t sector, uint16_t sector_count, void *dest);
 
 #endif
