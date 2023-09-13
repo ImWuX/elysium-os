6708d78a1110b034d730d23369a0283ed820c306
WuX <axel.riska@gmail.com>
2023-09-13 11:12:16 +0200

    MISC :: Backporting general changes made in the original feature/vfs branch

--- kernel/src/arch/amd64/gdt.c
+++ kernel/src/arch/amd64/gdt.c
@@ -1,7 +1,6 @@
 #include "gdt.h"
-// #include <string.h>
-// #include <memory/heap.h>
 
+#define RSP_SEGMENT 0x28
 typedef struct {
     uint16_t limit;
     uint16_t base_low;
@@ -23,8 +22,6 @@ typedef struct {
     uint16_t limit;
     uint64_t base;
 } __attribute__((packed)) gdt_descriptor_t;
-
-static gdt_tss_t g_tss;
 
 static gdt_entry_t g_gdt[] = {
     {},
@@ -70,23 +67,16 @@ void gdt_load() {
     asm volatile("lgdt %0\npush $0x8\nlea 1f(%%rip), %%rax\npush %%rax\nlretq\n1:\nmov $0x10, %%rax\nmov %%rax, %%ds\nmov %%rax, %%ss\nmov %%rax, %%es\nmov %%rax, %%fs\nmov %%rax, %%gs\n" : : "m" (gdtr) : "rax", "memory");
 }
 
-// void gdt_tss_initialize() {
-//     memset(&g_tss, 0, sizeof(g_tss));
+void gdt_load_tss(tss_t *tss) {
+    gdt_system_entry_t *entry = (gdt_system_entry_t *) ((uintptr_t) g_gdt + RSP_SEGMENT);
+    entry->entry.access = 0b10001001;
+    entry->entry.flags = (1 << 4) | (((uint8_t) (sizeof(tss_t) << 16)) & 0b00001111);
+    entry->entry.limit = (uint16_t) sizeof(tss_t);
+    entry->entry.base_low = (uint16_t) (uint64_t) tss;
+    entry->entry.base_mid = (uint8_t) ((uint64_t) tss >> 16);
+    entry->entry.base_high = (uint8_t) ((uint64_t) tss >> 24);
+    entry->base_ext = (uint32_t) ((uint64_t) tss >> 32);
 
-//     uint64_t rsp = (uint64_t) heap_alloc(0x5000) + 0x5000 - 1; // TODO: Very temporary way of allocating a stack
-//     g_tss.rsp0_lower = (uint32_t) rsp;
-//     g_tss.rsp0_upper = (uint32_t) (rsp >> 32);
-//     g_tss.iomap_base = sizeof(gdt_tss_t);
-
-//     uint16_t seg = 0x28;
-//     gdt_system_entry_t *entry = (gdt_system_entry_t *) ((uintptr_t) g_gdt + seg);
-//     entry->entry.access = 0b10001001;
-//     entry->entry.flags = (1 << 4) | (((uint8_t) (sizeof(g_tss) << 16)) & 0b00001111);
-//     entry->entry.limit = (uint16_t) sizeof(g_tss);
-//     entry->entry.base_low = (uint16_t) (uint64_t) &g_tss;
-//     entry->entry.base_mid = (uint8_t) ((uint64_t) &g_tss >> 16);
-//     entry->entry.base_high = (uint8_t) ((uint64_t) &g_tss >> 24);
-//     entry->base_ext = (uint32_t) ((uint64_t) &g_tss >> 32);
-
-//     asm volatile("ltr %0" : : "m" (seg));
-// }
+    uint16_t segment = RSP_SEGMENT;
+    asm volatile("ltr %0" : : "m" (segment));
+}
---
+++ kernel/src/arch/amd64/tss.c
@@ -0,0 +1,7 @@
+#include "tss.h"
+#include <string.h>
+
+void tss_set_rsp0(tss_t *tss, uintptr_t stack_pointer) {
+    tss->rsp0_lower = (uint32_t) (uint64_t) stack_pointer;
+    tss->rsp0_upper = (uint32_t) ((uint64_t) stack_pointer >> 32);
+}
