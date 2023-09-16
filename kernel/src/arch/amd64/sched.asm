d1d425911b183da83f6857193716ece11e6b1254
WuX <axel.riska@gmail.com>
2023-09-16 22:28:38 +0200

    SCHED :: Userspace works. Missing some important implementation to use it properly.

--- kernel/src/arch/amd64/sched.asm
+++ kernel/src/arch/amd64/sched.asm
@@ -27,5 +27,18 @@ sched_userspace_init:
     swapgs
     pop rcx
 
+    xor rax, rax
+    xor rbx, rbx
+	xor rdx, rdx
+	xor rsi, rsi
+	xor rdi, rdi
+	xor r8, r8
+	xor r9, r9
+	xor r10, r10
+	xor r12, r12
+	xor r13, r13
+	xor r14, r14
+	xor r15, r15
+
     mov r11, 0x202
     sysretq
--- kernel/src/arch/amd64/sched.c
+++ kernel/src/arch/amd64/sched.c
@@ -16,33 +16,48 @@
 #include <arch/amd64/lapic.h>
 
 #define KERNEL_STACK_SIZE_PG 4
+#define USER_STACK_SIZE_PG 8
 #define ARCH_THREAD(THREAD) (container_of(THREAD, arch_thread_t, common))
 
 typedef struct {
-    uintptr_t rsp;
     uintptr_t base;
     uintptr_t size;
 } stack_t;
 
 typedef struct arch_thread {
     struct arch_thread *this;
+    uintptr_t rsp;
     stack_t kernel_stack;
+    stack_t user_stack;
     thread_t common;
 } arch_thread_t;
 
 typedef struct {
     uint64_t r15, r14, r13, r12, rbp, rbx;
     void (* thread_init)(arch_thread_t *prev);
-    void (* thread_func)();
+    void (* thread_init_kernel)();
+    void (* entry)();
     struct {
         uint64_t rbp;
         uint64_t rip;
     } invalid_stack_frame;
-} init_stack_t;
+} init_stack_kernel_t;
 
-static_assert(offsetof(arch_thread_t, kernel_stack) + offsetof(stack_t, rsp) == 8, "Kernel RSP in thread_t changed. Update arch/amd64/sched.asm");
+typedef struct {
+    uint64_t r15, r14, r13, r12, rbp, rbx;
+    void (* thread_init)(arch_thread_t *prev);
+    void (* thread_init_user)();
+    void (* entry)();
+    struct {
+        uint64_t rbp;
+        uint64_t rip;
+    } invalid_stack_frame;
+} init_stack_user_t;
+
+static_assert(offsetof(arch_thread_t, rsp) == 8, "Kernel RSP in thread_t changed. Update arch/amd64/sched.asm");
 
 extern arch_thread_t *sched_context_switch(arch_thread_t *this, arch_thread_t *next);
+extern void sched_userspace_init();
 
 static long g_next_tid = 1;
 static int g_sched_vector = 0;
@@ -53,11 +68,13 @@ static int g_sched_vector = 0;
     will stay in RDI throughout the asm routine and will still
     be present upon entry here
 */
-static void sched_thread_init(arch_thread_t *prev) {
+static void common_thread_init(arch_thread_t *prev) {
     sched_thread_drop(&prev->common);
 
     lapic_timer_oneshot(g_sched_vector, 1'000'000);
+}
 
+static void kernel_thread_init() {
     asm volatile("sti");
 }
 
@@ -83,31 +100,71 @@ static void sched_switch(arch_thread_t *this, arch_thread_t *next) {
     sched_thread_drop(&prev->common);
 }
 
-static stack_t allocate_init_stack(uint64_t stack_page_size, void (* func)()) {
-    pmm_page_t *stack_page = pmm_alloc_pages(stack_page_size, PMM_GENERAL | PMM_AF_ZERO);
-    init_stack_t *init_stack = (init_stack_t *) HHDM(stack_page->paddr + stack_page_size * ARCH_PAGE_SIZE - sizeof(init_stack_t));
-    init_stack->thread_init = &sched_thread_init;
-    init_stack->thread_func = func;
-    return (stack_t) {
-        .rsp = (uintptr_t) init_stack,
-        .base = HHDM(stack_page->paddr + stack_page_size * ARCH_PAGE_SIZE),
-        .size = stack_page_size * ARCH_PAGE_SIZE
-    };
-}
-
-static arch_thread_t *create_thread(vmm_address_space_t *address_space, stack_t kernel_stack) {
+static arch_thread_t *create_thread(vmm_address_space_t *address_space, stack_t kernel_stack, stack_t user_stack, uintptr_t rsp) {
     arch_thread_t *thread = heap_alloc(sizeof(arch_thread_t));
     memset(thread, 0, sizeof(arch_thread_t));
     thread->this = thread;
     thread->common.id = g_next_tid++;
     thread->common.state = THREAD_STATE_READY;
     thread->common.address_space = address_space;
+    thread->rsp = rsp;
     thread->kernel_stack = kernel_stack;
+    thread->user_stack = user_stack;
     return thread;
 }
 
 static arch_thread_t *create_kernel_thread(void (* func)()) {
-    arch_thread_t *thread = create_thread(&g_kernel_address_space, allocate_init_stack(KERNEL_STACK_SIZE_PG, func));
+    pmm_page_t *kernel_stack_page = pmm_alloc_pages(KERNEL_STACK_SIZE_PG, PMM_GENERAL | PMM_AF_ZERO);
+    stack_t kernel_stack = {
+        .base = HHDM(kernel_stack_page->paddr + KERNEL_STACK_SIZE_PG * ARCH_PAGE_SIZE),
+        .size = KERNEL_STACK_SIZE_PG * ARCH_PAGE_SIZE
+    };
+
+    init_stack_kernel_t *init_stack = (init_stack_kernel_t *) (kernel_stack.base - sizeof(init_stack_kernel_t));
+    init_stack->entry = func;
+    init_stack->thread_init = &common_thread_init;
+    init_stack->thread_init_kernel = &kernel_thread_init;
+
+    arch_thread_t *thread = create_thread(&g_kernel_address_space, kernel_stack, (stack_t) {}, (uintptr_t) init_stack);
+    list_insert_behind(&g_sched_threads_all, &thread->common.list_all);
+    return thread;
+}
+
+static arch_thread_t *create_user_thread() { // TODO: This is very much a test for userspace threads at this point
+    vmm_address_space_t *as = heap_alloc(sizeof(vmm_address_space_t));
+    pmm_page_t *pml4 = pmm_alloc_page(PMM_GENERAL | PMM_AF_ZERO);
+    for(int i = 256; i < 512; i++) {
+        ((uint64_t *) HHDM(pml4->paddr))[i] = ((uint64_t *) HHDM(g_kernel_address_space.archdep.cr3))[i];
+    }
+    as->archdep.cr3 = pml4->paddr;
+    as->lock = SLOCK_INIT;
+    as->segments = LIST_INIT;
+
+    pmm_page_t *kernel_stack_page = pmm_alloc_pages(KERNEL_STACK_SIZE_PG, PMM_GENERAL | PMM_AF_ZERO);
+    stack_t kernel_stack = {
+        .base = HHDM(kernel_stack_page->paddr + KERNEL_STACK_SIZE_PG * ARCH_PAGE_SIZE),
+        .size = KERNEL_STACK_SIZE_PG * ARCH_PAGE_SIZE
+    };
+
+    pmm_page_t *user_stack_page = pmm_alloc_pages(USER_STACK_SIZE_PG, PMM_GENERAL | PMM_AF_ZERO);
+    stack_t user_stack = {
+        .base = ARCH_PAGE_SIZE * (USER_STACK_SIZE_PG + 1),
+        .size = USER_STACK_SIZE_PG * ARCH_PAGE_SIZE
+    };
+    for(int i = 0; i < USER_STACK_SIZE_PG; i++) {
+        arch_vmm_map(as, ARCH_PAGE_SIZE * (i + 1), user_stack_page->paddr + i * ARCH_PAGE_SIZE, VMM_FLAGS_WRITE | VMM_FLAGS_USER);
+    }
+
+    pmm_page_t *program_page = pmm_alloc_page(PMM_GENERAL | PMM_AF_ZERO);
+    arch_vmm_map(as, 0, program_page->paddr, VMM_FLAGS_EXEC | VMM_FLAGS_USER | VMM_FLAGS_WRITE);
+    *(uint32_t *) (HHDM(program_page->paddr)) = 0xFEEB;
+
+    init_stack_user_t *init_stack = (init_stack_user_t *) HHDM(user_stack_page->paddr + USER_STACK_SIZE_PG * ARCH_PAGE_SIZE - sizeof(init_stack_user_t));
+    init_stack->entry = 0;
+    init_stack->thread_init = &common_thread_init;
+    init_stack->thread_init_user = &sched_userspace_init;
+
+    arch_thread_t *thread = create_thread(as, kernel_stack, user_stack, user_stack.base - sizeof(init_stack_user_t));
     list_insert_behind(&g_sched_threads_all, &thread->common.list_all);
     return thread;
 }
@@ -136,13 +193,8 @@ thread_t *arch_sched_current_thread() {
 }
 
 [[noreturn]] void sched_init_cpu(arch_cpu_t *cpu) {
-    arch_thread_t *idle_thread = heap_alloc(sizeof(arch_thread_t));
-    idle_thread->this = idle_thread;
+    arch_thread_t *idle_thread = create_kernel_thread(&sched_idle);
     idle_thread->common.id = 0;
-    idle_thread->common.state = THREAD_STATE_READY;
-    idle_thread->common.address_space = &g_kernel_address_space;
-    idle_thread->common.cpu = 0;
-    idle_thread->kernel_stack = allocate_init_stack(KERNEL_STACK_SIZE_PG, &sched_idle);
     cpu->common.idle_thread = &idle_thread->common;
 
     arch_thread_t *dummy_thread = heap_alloc(sizeof(arch_thread_t));
@@ -154,58 +206,8 @@ thread_t *arch_sched_current_thread() {
     __builtin_unreachable();
 }
 
-static void temptestfunc() {
-    while(true) {
-        for(int i = 0; i < 100000000; i++);
-        // kprintf("%lu, ", arch_sched_current_thread()->id);
-    }
-}
-
 void sched_init() {
     int sched_vector = interrupt_request(INTERRUPT_PRIORITY_SCHED, &sched_entry);
     ASSERTC(sched_vector >= 0, "Unable to acquire an interrupt vector for the scheduler");
     g_sched_vector = sched_vector;
-
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
-    sched_thread_schedule(&create_kernel_thread(&temptestfunc)->common);
 }
