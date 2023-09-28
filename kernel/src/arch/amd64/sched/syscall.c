fd967db877e8c5eee9b9851fea3c72c1f72721e0
WuX <axel.riska@gmail.com>
2023-09-28 13:03:41 +0200

    GENERAL :: Loading the file styx.elf on startup. Temporary syscall for acquiring the framebuffer from userspace. Some refactoring on SCHED.

--- kernel/src/arch/amd64/sched/syscall.c
+++ kernel/src/arch/amd64/sched/syscall.c
@@ -1,15 +1,21 @@
 #include "syscall.h"
+#include <lib/assert.h>
 #include <lib/kprint.h>
+#include <memory/heap.h>
+#include <memory/hhdm.h>
 #include <sched/sched.h>
 #include <arch/sched.h>
+#include <arch/vmm.h>
 #include <arch/amd64/sched/sched.h>
 #include <arch/amd64/msr.h>
 #include <arch/amd64/gdt.h>
+#include <graphics/draw.h>
 
-#define MAX_HANDLERS 2
 #define MSR_EFER_SCE 1
 
 extern void syscall_entry();
+
+extern draw_context_t g_fb_context;
 
 int64_t syscall_exit() {
     arch_sched_thread_current()->state = THREAD_STATE_DESTROY;
@@ -19,6 +25,26 @@ int64_t syscall_exit() {
 
 int64_t syscall_write(uint64_t ch) {
     putchar((int) ch);
+    return 0;
+}
+
+typedef struct {
+    uint64_t addr;
+    uint64_t pitch;
+    uint64_t width;
+    uint64_t height;
+} elib_fb_t;
+
+int64_t syscall_fb(uint64_t addr, elib_fb_t *fb) {
+    ASSERTC(arch_sched_thread_current()->proc, "Should be a userspace thread");
+    uint64_t mapsz = g_fb_context.pitch * g_fb_context.height * sizeof(uint32_t);
+    for(uint64_t i = 0; i < (mapsz + ARCH_PAGE_SIZE - 1) / ARCH_PAGE_SIZE; i++) {
+        arch_vmm_map(arch_sched_thread_current()->proc->address_space, addr + i * ARCH_PAGE_SIZE, (uintptr_t) g_fb_context.address - g_hhdm_address + i * ARCH_PAGE_SIZE, VMM_FLAGS_USER | VMM_FLAGS_WRITE);
+    }
+    fb->addr = addr;
+    fb->width = g_fb_context.width;
+    fb->height = g_fb_context.height;
+    fb->pitch = g_fb_context.pitch;
     return 0;
 }
 
