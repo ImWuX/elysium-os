7bc311b0348db7e4fa7fb8559ba31b5453bcf3dd
WuX <wux@thenest.dev>
2023-12-30 01:32:25 -0800

    logging: implemented kprint

--- kernel/src/arch/x86_64/init.c
+++ kernel/src/arch/x86_64/init.c
@@ -1,6 +1,16 @@
 #include <tartarus.h>
+#include <lib/kprint.h>
+#include <arch/x86_64/port.h>
+
+static void pch(char ch) {
+    port_outb(0x3F8, ch);
+}
 
 [[noreturn]] void init(tartarus_boot_info_t *boot_info) {
+    g_kprint_putchar = pch;
+
+    kprintf("Hello World\n");
+
     for(;;);
     __builtin_unreachable();
 }
