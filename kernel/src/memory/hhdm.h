ca0ca329db589a4627565ee5b7338ed2a4f5738f
WuX <axel.riska@gmail.com>
2023-12-30 18:57:52 -0800

    memory: added hhdm

---
+++ kernel/src/memory/hhdm.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdint.h>
+
+/**
+ * @brief Converts a physical address to a virtual HHDM address.
+ *
+ * @param address physical address
+ * @return virtual address in the HHDM
+ */
+#define HHDM(ADDRESS) ((uintptr_t) (ADDRESS) + g_hhdm_base)
+
+extern uintptr_t g_hhdm_base;
