c45d408428095c359afdee6f8a0bdd3bf3ad8cdf
WuX <wux@thenest.dev>
2023-12-30 18:57:52 -0800

    memory: added hhdm

--- kernel/src/arch/x86_64/init.c
+++ kernel/src/arch/x86_64/init.c
@@ -1,12 +1,17 @@
 #include <tartarus.h>
+#include <memory/hhdm.h>
 #include <lib/kprint.h>
 #include <arch/x86_64/port.h>
+
+uintptr_t g_hhdm_base;
 
 static void pch(char ch) {
     port_outb(0x3F8, ch);
 }
 
 [[noreturn]] void init(tartarus_boot_info_t *boot_info) {
+    g_hhdm_base = boot_info->hhdm_base;
+
     g_kprint_putchar = pch;
 
     kprintf("Hello World\n");
