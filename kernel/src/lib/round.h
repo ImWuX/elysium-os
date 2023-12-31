c1d3c70b2948fe4edc0ae21ac27dcb4f885971de
WuX <axel.riska@gmail.com>
2023-12-30 22:37:59 -0800

    misc: added round helpers

---
+++ kernel/src/lib/round.h
@@ -0,0 +1,25 @@
+#pragma once
+
+/**
+ * @brief Divide and round up.
+ * @param DIVIDEND
+ * @param DIVISOR
+ * @returns result
+ */
+#define ROUND_DIV_UP(DIVIDEND, DIVISOR) (((DIVIDEND) + (DIVISOR) - 1) / (DIVISOR))
+
+/**
+ * @brief Round up.
+ * @param VALUE
+ * @param PRECISION
+ * @returns result
+ */
+#define ROUND_UP(VALUE, PRECISION) (ROUND_DIV_UP((VALUE), (PRECISION)) * (PRECISION))
+
+/**
+ * @brief Round down.
+ * @param VALUE
+ * @param PRECISION
+ * @returns result
+ */
+#define ROUND_DOWN(VALUE, PRECISION) (((VALUE) / (PRECISION)) * (PRECISION))
