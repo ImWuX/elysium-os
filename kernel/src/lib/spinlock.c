de4ef3bd7ab40990c04ee1795c24dc98dd2ef4c7
WuX <axel.riska@gmail.com>
2023-12-30 22:34:11 -0800

    spinlock: refactored slock to spinlock

--- kernel/src/lib/spinlock.c
+++ kernel/src/lib/spinlock.c
@@ -1,11 +1,11 @@
-#include "slock.h"
+#include "spinlock.h"
 #include <stdint.h>
 #include <lib/assert.h>
 #include <arch/cpu.h>
 
 #define DEADLOCK_AT 100000000
 
-void spinlock_acquire(volatile slock_t *lock) {
+void spinlock_acquire(volatile spinlock_t *lock) {
     uint64_t dead = 0;
     for(;;) {
         if(spinlock_try_acquire(lock)) return;
--- kernel/src/lib/spinlock.h
+++ kernel/src/lib/spinlock.h
@@ -1,15 +1,15 @@
 #pragma once
 
-#define SLOCK_INIT 0
+#define SPINLOCK_INIT 0
 
-typedef bool slock_t;
+typedef bool spinlock_t;
 
 /**
  * @brief Acquire a spinlock.
  * @warning Spins until acquired.
  * @param lock
  */
-void spinlock_acquire(volatile slock_t *lock);
+void spinlock_acquire(volatile spinlock_t *lock);
 
 /**
  * @brief Try to acquire a spinlock.
@@ -17,7 +17,7 @@ void spinlock_acquire(volatile slock_t *lock);
  * @param lock
  * @returns true = acquired the lock
  */
-static inline bool spinlock_try_acquire(volatile slock_t *lock) {
+static inline bool spinlock_try_acquire(volatile spinlock_t *lock) {
     return  !__atomic_test_and_set(lock, __ATOMIC_ACQUIRE);
 }
 
@@ -25,6 +25,6 @@ static inline bool spinlock_try_acquire(volatile slock_t *lock) {
  * @brief Release a spinlock.
  * @param lock
  */
-static inline void spinlock_release(volatile slock_t *lock) {
+static inline void spinlock_release(volatile spinlock_t *lock) {
     __atomic_clear(lock, __ATOMIC_RELEASE);
 }
