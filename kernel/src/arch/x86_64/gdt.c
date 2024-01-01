96c5bad97afe19140dd2859ea303898814e0e958
WuX <axel.riska@gmail.com>
2023-12-31 23:45:15 -0800

    x86_64: added gdt

---
+++ kernel/src/arch/x86_64/gdt.c
@@ -0,0 +1,72 @@
+#include "gdt.h"
+
+typedef struct {
+    uint16_t limit;
+    uint16_t base_low;
+    uint8_t base_mid;
+    uint8_t access;
+    uint8_t flags;
+    uint8_t base_high;
+} __attribute__((packed)) gdt_entry_t;
+
+typedef struct {
+    uint16_t limit;
+    uint64_t base;
+} __attribute__((packed)) gdt_descriptor_t;
+
+static gdt_entry_t g_gdt[] = {
+    {},
+    {
+        .limit = 0,
+        .base_low = 0,
+        .base_mid = 0,
+        .access = 0b10011010,
+        .flags = 0b00100000,
+        .base_high = 0
+    },
+    {
+        .limit = 0,
+        .base_low = 0,
+        .base_mid = 0,
+        .access = 0b10010010,
+        .flags = 0b00000000,
+        .base_high = 0
+    },
+    {
+        .limit = 0,
+        .base_low = 0,
+        .base_mid = 0,
+        .access = 0b11110010,
+        .flags = 0b00000000,
+        .base_high = 0
+    },
+    {
+        .limit = 0,
+        .base_low = 0,
+        .base_mid = 0,
+        .access = 0b11111010,
+        .flags = 0b00100000,
+        .base_high = 0
+    }
+};
+
+void gdt_load() {
+    gdt_descriptor_t gdtr;
+    gdtr.limit = sizeof(g_gdt) - 1;
+    gdtr.base = (uint64_t) &g_gdt;
+    asm volatile(
+        "lgdt %0\n"
+        "push $0x8\n"
+        "lea 1f(%%rip), %%rax\n"
+        "push %%rax\n"
+        "lretq\n"
+        "1:\n"
+        "mov $0x10, %%rax\n"
+        "mov %%rax, %%ds\n"
+        "mov %%rax, %%ss\n"
+        "mov %%rax, %%es\n"
+        "mov %%rax, %%fs\n"
+        "mov %%rax, %%gs\n"
+        : : "m" (gdtr) : "rax", "memory"
+    );
+}
