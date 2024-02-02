e3a729209ae6d8c83a3a1038a865c85066bd0f66
WuX <wux@thenest.dev>
2024-02-02 18:17:56 +0100

    misc: added "X86_64_" prefix to some enums I missed

--- kernel/src/arch/x86_64/msr.h
+++ kernel/src/arch/x86_64/msr.h
@@ -2,16 +2,16 @@
 #include <stdint.h>
 
 typedef enum {
-    MSR_APIC_BASE       = 0x1B,
-    MSR_PAT             = 0x277,
-    MSR_EFER            = 0xC0000080,
-    MSR_STAR            = 0xC0000081,
-    MSR_LSTAR           = 0xC0000082,
-    MSR_CSTAR           = 0xC0000083,
-    MSR_SFMASK          = 0xC0000084,
-    MSR_FS_BASE         = 0xC0000100,
-    MSR_GS_BASE         = 0xC0000101,
-    MSR_KERNEL_GS_BASE  = 0xC0000102
+    X86_64_MSR_APIC_BASE       = 0x1B,
+    X86_64_MSR_PAT             = 0x277,
+    X86_64_MSR_EFER            = 0xC0000080,
+    X86_64_MSR_STAR            = 0xC0000081,
+    X86_64_MSR_LSTAR           = 0xC0000082,
+    X86_64_MSR_CSTAR           = 0xC0000083,
+    X86_64_MSR_SFMASK          = 0xC0000084,
+    X86_64_MSR_FS_BASE         = 0xC0000100,
+    X86_64_MSR_GS_BASE         = 0xC0000101,
+    X86_64_MSR_KERNEL_GS_BASE  = 0xC0000102
 } x86_64_msr_t;
 
 /**
