dc719ce9ed4469ed571c00892be6d4bb2c83e41b
WuX <axel.riska@gmail.com>
2024-02-20 00:46:16 +0100

    common: removed deprecated code

--- kernel/src/common/kprint.h
+++ kernel/src/common/kprint.h
@@ -2,10 +2,6 @@
 #include <stdint.h>
 #include <stdarg.h>
 
-typedef void (*kprint_putchar_t)(char ch);
-
-extern kprint_putchar_t g_kprint_putchar;
-
 /**
  * @brief Prints a formatted string
  * @returns char count written
