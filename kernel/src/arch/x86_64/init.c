5a7dc369bb9ab645867ec16c73f06a1432fc3751
WuX <wux@thenest.dev>
2024-02-20 00:10:48 +0100

    arch: added cpu

--- kernel/src/arch/x86_64/init.c
+++ kernel/src/arch/x86_64/init.c
@@ -1,16 +1,11 @@
 #include <tartarus.h>
+#include <arch/cpu.h>
 
 static void pch(char ch) {
     asm volatile("outb %0, %1" : : "a" (ch), "Nd" (0x3F8));
 }
 
-static void pstr(char *str) {
-    while(*str != 0) pch(*str++);
-}
-
 [[noreturn]] void init(tartarus_boot_info_t *boot_info) {
-    pstr("Hello World!\n");
 
-    for(;;) asm volatile("hlt");
-    __builtin_unreachable();
+    arch_cpu_halt();
 }
