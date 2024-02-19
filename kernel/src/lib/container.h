d3cac838e5ee0806188747e162173bd9bafe4e3d
WuX <axel.riska@gmail.com>
2024-02-19 01:04:19 +0100

    lib: generic lib stuff

---
+++ kernel/src/lib/container.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/**
+ * @brief Get the container of a child struct
+ * @param PTR pointer to the child struct
+ * @param TYPE type of the container
+ * @param MEMBER name of the child member in the container
+ */
+#define CONTAINER_OF(PTR, TYPE, MEMBER) ({ const typeof( ((TYPE *)0)->MEMBER ) *__mptr = (PTR); (TYPE *)( (char *)__mptr - __builtin_offsetof(TYPE,MEMBER) );})
