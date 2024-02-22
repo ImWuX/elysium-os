355538c8f8b68246f93ab3c14383ea4244f85592
WuX <axel.riska@gmail.com>
2024-02-22 22:50:29 +0100

    arch/x86_64: added tss to sched

--- kernel/src/arch/x86_64/sched.h
+++ kernel/src/arch/x86_64/sched.h
@@ -10,3 +10,9 @@ void x86_64_sched_init();
  * @brief Sets up a new CPU for scheduling
  */
 [[noreturn]] void x86_64_sched_init_cpu(x86_64_cpu_t *cpu);
+
+/**
+ * @brief Switch to the next thread
+ * @warning This essentially yields to the next thread, without the yield logic
+ */
+void x86_64_sched_next();
