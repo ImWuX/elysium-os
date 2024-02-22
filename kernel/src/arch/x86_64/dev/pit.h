45813a27a1193a3f6aa1e86ebe4a7fb93739936f
WuX <wux@thenest.dev>
2024-02-22 05:24:08 +0100

    arch/x86_64: added pit

---
+++ kernel/src/arch/x86_64/dev/pit.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <stdint.h>
+
+#define PIT_FREQ 1193182
+
+/**
+ * @brief Initialized the PIT
+ * @param divisor PIT divisor
+ */
+void pit_initialize(uint16_t divisor);
+
+/**
+ * @brief Retrieve current PIT count
+ * @return Current PIT count
+ */
+uint16_t pit_count();
