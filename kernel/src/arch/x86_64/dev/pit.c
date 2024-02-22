66cd1a5c50310e0a9d4349a2481ab67e27029589
WuX <axel.riska@gmail.com>
2024-02-22 05:24:08 +0100

    arch/x86_64: added pit

---
+++ kernel/src/arch/x86_64/dev/pit.c
@@ -0,0 +1,20 @@
+#include "pit.h"
+#include <arch/x86_64/port.h>
+
+#define CHANNEL0_DATA 0x40
+#define CHANNEL1_DATA 0x41
+#define CHANNEL2_DATA 0x42
+#define CMD 0x43
+
+void pit_initialize(uint16_t divisor) {
+    x86_64_port_outb(CMD, 0x34);
+    x86_64_port_outb(CHANNEL0_DATA, (uint8_t) divisor);
+    x86_64_port_outb(CHANNEL0_DATA, (uint8_t) (divisor >> 8));
+}
+
+uint16_t pit_count() {
+    x86_64_port_outb(CMD, 0);
+    uint16_t low = x86_64_port_inb(CHANNEL0_DATA);
+    uint16_t high = x86_64_port_inb(CHANNEL0_DATA);
+    return low | (high << 8);
+}
