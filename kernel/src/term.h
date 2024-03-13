d0c70b300b958c6bf7375c118f16b2723b9d5ed2
WuX <axel.riska@gmail.com>
2024-03-13 12:22:42 +0100

    term: forgot header declaration

--- kernel/src/term.h
+++ kernel/src/term.h
@@ -7,6 +7,11 @@
 void term_init(draw_context_t *context);
 
 /**
+ * @brief Close the terminal
+ */
+void term_close();
+
+/**
  * @brief Hook for handling keystrokes
  */
 void term_kb_handler(uint8_t ch);
