e9c899e18b6b80ec26d03bfbdcb09eff4362ce0e
WuX <axel.riska@gmail.com>
2024-03-13 12:05:00 +0100

    vmm: add zeroing to anon allocations for userspace

--- kernel/src/memory/vmm/seg_anon.c
+++ kernel/src/memory/vmm/seg_anon.c
@@ -4,15 +4,19 @@
 #include <arch/vmm.h>
 #include <arch/types.h>
 
+#define HAS_FLAG(SEG, FLAG) ((SEG)->driver_data != NULL && (((uint64_t) (SEG)->driver_data) & (FLAG)) != 0)
+
 static void map(vmm_segment_t *segment, uintptr_t address) {
-    int flags = ARCH_VMM_FLAG_NONE;
-    if(segment->address_space != g_vmm_kernel_address_space) flags |= ARCH_VMM_FLAG_USER;
-    pmm_page_t *page = pmm_alloc_page(PMM_STANDARD);
-    arch_vmm_map(segment->address_space, address, page->paddr, segment->protection, flags);
+    int pmm_flags = PMM_STANDARD;
+    int vmm_flags = ARCH_VMM_FLAG_NONE;
+    if(segment->address_space != g_vmm_kernel_address_space) vmm_flags |= ARCH_VMM_FLAG_USER;
+    if(HAS_FLAG(segment, SEG_ANON_FLAG_ZERO)) vmm_flags |= PMM_FLAG_ZERO;
+    pmm_page_t *page = pmm_alloc_page(pmm_flags);
+    arch_vmm_map(segment->address_space, address, page->paddr, segment->protection, vmm_flags);
 }
 
 static void seg_attach(vmm_segment_t *segment) {
-    if(segment->driver_data == NULL || !((bool) segment->driver_data)) return;
+    if(!HAS_FLAG(segment, SEG_ANON_FLAG_PREALLOC)) return;
     for(uintptr_t length = 0; length < segment->length; length += ARCH_PAGE_SIZE) map(segment, segment->base + length);
 }
 
