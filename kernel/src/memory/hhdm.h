5abafcfb7e56410de30bf138c376aaabdda73ec4
WuX <axel.riska@gmail.com>
2024-03-13 20:33:56 +0100

    hhdm: added a hhdm to phys macro

--- kernel/src/memory/hhdm.h
+++ kernel/src/memory/hhdm.h
@@ -9,5 +9,12 @@
  */
 #define HHDM(ADDRESS) ((uintptr_t) (ADDRESS) + g_hhdm_base)
 
+/**
+ * @brief Converts a virtual HHDM address to a physical address
+ * @param ADDRESS virtual address inside the HHDM
+ * @returns physical address
+ */
+#define HHDM_TO_PHYS(ADDRESS) ((uintptr_t) (ADDRESS) - g_hhdm_base)
+
 extern uintptr_t g_hhdm_base;
 extern size_t g_hhdm_size; // TODO: Maybe do some bounds checking here. maybe under a debug flag?
