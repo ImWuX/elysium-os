248b02c1eadd72faf4ba5df5a81f75cd28279467
WuX <axel.riska@gmail.com>
2024-03-14 20:29:54 +0100

    sys: added timers

--- kernel/src/sys/time.c
+++ kernel/src/sys/time.c
@@ -1,14 +1,33 @@
 #include "time.h"
-#include <common/log.h>
 #include <common/spinlock.h>
+#include <memory/heap.h>
 
-static time_t g_monotonic_time = {};
+time_t g_monotonic_time = {};
 static spinlock_t g_lock = SPINLOCK_INIT;
+static list_t g_timers = LIST_INIT; // TODO: this needs to be a vector instead of a list
 
 void time_advance(time_t length) {
     spinlock_acquire(&g_lock);
 
     g_monotonic_time = time_add(g_monotonic_time, length);
 
+    LIST_FOREACH(&g_timers, elem) {
+        timer_t *timer = LIST_CONTAINER_GET(elem, timer_t, list_elem);
+        if(timer->deadline.seconds > g_monotonic_time.seconds) continue;
+        if(timer->deadline.seconds == g_monotonic_time.seconds && timer->deadline.nanoseconds > g_monotonic_time.nanoseconds) continue;
+        list_delete(&timer->list_elem);
+        timer->callback(timer);
+    }
+
     spinlock_release(&g_lock);
+}
+
+timer_t *timer_create(time_t length, void (* callback)(timer_t *timer)) {
+    timer_t *timer = heap_alloc(sizeof(timer_t));
+    timer->callback = callback;
+    spinlock_acquire(&g_lock);
+    timer->deadline = time_add(g_monotonic_time, length);
+    list_append(&g_timers, &timer->list_elem);
+    spinlock_release(&g_lock);
+    return timer;
 }
--- kernel/src/sys/time.h
+++ kernel/src/sys/time.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <stdint.h>
+#include <lib/list.h>
 
 #define TIME_NANOSECONDS_IN_SECOND 1'000'000'000
 
@@ -7,6 +8,14 @@ typedef struct {
     uint64_t seconds;
     uint32_t nanoseconds;
 } time_t;
+
+typedef struct timer {
+    time_t deadline;
+    void (* callback)(struct timer *timer);
+    list_element_t list_elem;
+} timer_t;
+
+extern time_t g_monotonic_time;
 
 /**
  * @brief Add a and b
@@ -48,3 +57,8 @@ static inline time_t time_subtract(time_t a, time_t b) {
  * @brief Advance time by some length
  */
 void time_advance(time_t length);
+
+/**
+ * @brief Create timer
+ */
+timer_t *timer_create(time_t length, void (* callback)(timer_t *timer));
