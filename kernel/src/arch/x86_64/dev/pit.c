67c75d3ce90bbe26eb2c415e6a70e9e3c4af87c0
WuX <axel.riska@gmail.com>
2024-03-14 19:45:33 +0100

    arch/x86_64: added ability to set frequence for pit & renamed set_divisor to set_reload

--- kernel/src/arch/x86_64/dev/pit.c
+++ kernel/src/arch/x86_64/dev/pit.c
@@ -6,10 +6,16 @@
 #define CHANNEL2_DATA 0x42
 #define CMD 0x43
 
-void x86_64_pit_initialize(uint16_t divisor) {
+void x86_64_pit_set_reload(uint16_t reload_value) {
     x86_64_port_outb(CMD, 0x34);
-    x86_64_port_outb(CHANNEL0_DATA, (uint8_t) divisor);
-    x86_64_port_outb(CHANNEL0_DATA, (uint8_t) (divisor >> 8));
+    x86_64_port_outb(CHANNEL0_DATA, (uint8_t) reload_value);
+    x86_64_port_outb(CHANNEL0_DATA, (uint8_t) (reload_value >> 8));
+}
+
+void x86_64_pit_set_frequency(uint64_t frequency) {
+    uint64_t divider = PIT_FREQ / frequency;
+    if(PIT_FREQ % frequency > frequency / 2) divider++;
+    x86_64_pit_set_reload((uint16_t) divider);
 }
 
 uint16_t x86_64_pit_count() {
