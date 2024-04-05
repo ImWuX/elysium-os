cca7e25a298268088a65e02918fd99a44aa75aac
WuX <wux@thenest.dev>
2024-04-06 01:42:37 +0200

    lib: added memmove and weak attributes to allow for optimizations in assembly

--- kernel/src/lib/mem.h
+++ kernel/src/lib/mem.h
@@ -11,13 +11,22 @@
 void *memset(void *dest, int ch, size_t count);
 
 /**
- * @brief Copy memory
+ * @brief Copy memory. dest and src are not allowed to overlap
  * @param dest
  * @param src
  * @param count amount of memory to copy
  * @returns destination
  */
 void *memcpy(void *dest, const void *src, size_t count);
+
+/**
+ * @brief Copy memory. dest and src are allowed to overlap
+ * @param dest
+ * @param src
+ * @param count amount of memory to move
+ * @returns destination
+ */
+void *memmove(void *dest, const void *src, size_t count);
 
 /**
  * @brief Compare two regions of memory. Results in a negative value if the LHS is greater, a positive value if the RHS is greater, and a zero if they're equal
