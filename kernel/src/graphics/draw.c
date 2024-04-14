78a8b5c2d950d55595c9ae2703e9b29df119ce36
WuX <axel.riska@gmail.com>
2024-04-14 12:02:58 +0200

    graphics: fixed issues related to pitch

--- kernel/src/graphics/draw.c
+++ kernel/src/graphics/draw.c
@@ -2,13 +2,13 @@
 #include <graphics/font.h>
 
 static inline void putpixel(draw_context_t *ctx, uint64_t offset, draw_color_t color) {
-    if(offset > ctx->height * ctx->pitch * sizeof(draw_color_t)) return;
-    ((draw_color_t *) ctx->address)[offset] = color;
+    if(offset > ctx->height * ctx->pitch) return;
+    *(draw_color_t *) ((uintptr_t) ctx->address + offset) = color;
 }
 
 static inline draw_color_t getpixel(draw_context_t *ctx, uint64_t offset) {
-    if(offset > ctx->height * ctx->pitch * sizeof(draw_color_t)) return 0;
-    return ((draw_color_t *) ctx->address)[offset];
+    if(offset > ctx->height * ctx->pitch) return 0;
+    return *(draw_color_t *) (ctx->address + offset);
 }
 
 draw_color_t draw_color(uint8_t r, uint8_t g, uint8_t b) {
@@ -16,7 +16,7 @@ draw_color_t draw_color(uint8_t r, uint8_t g, uint8_t b) {
 }
 
 draw_color_t draw_getpixel(draw_context_t *ctx, int x, int y) {
-    return getpixel(ctx, y * ctx->pitch + x);
+    return getpixel(ctx, y * ctx->pitch + x * sizeof(draw_color_t));
 }
 
 void draw_char(draw_context_t *ctx, int x, int y, char c, font_t *font, draw_color_t color) {
@@ -34,12 +34,10 @@ void draw_char(draw_context_t *ctx, int x, int y, char c, font_t *font, draw_col
     }
     uint8_t *font_char = &font->data[((unsigned int) (uint8_t) c) * font->width * font->height / 8];
 
-    uint64_t offset = x + y * ctx->pitch;
+    uint64_t offset = x * sizeof(draw_color_t) + y * ctx->pitch;
     for(int yy = 0; yy < h && y + yy < ctx->height; yy++) {
         for(int xx = 0; xx < w && x + xx < ctx->width; xx++) {
-            if(font_char[yy] & (1 << (w - xx))) {
-                putpixel(ctx, offset + xx, color);
-            }
+            if(font_char[yy] & (1 << (w - xx))) putpixel(ctx, offset + xx * sizeof(draw_color_t), color);
         }
         offset += ctx->pitch;
     }
@@ -54,7 +52,7 @@ void draw_string_simple(draw_context_t *ctx, int x, int y, char *str, font_t *fo
 
 void draw_pixel(draw_context_t *ctx, int x, int y, draw_color_t color) {
     if(x < 0 || y < 0 || x >= ctx->width || y >= ctx->height) return;
-    putpixel(ctx, y * ctx->pitch + x, color);
+    putpixel(ctx, y * ctx->pitch + x * sizeof(draw_color_t), color);
 }
 
 void draw_rect(draw_context_t *ctx, int x, int y, uint16_t w, uint16_t h, draw_color_t color) {
@@ -68,10 +66,10 @@ void draw_rect(draw_context_t *ctx, int x, int y, uint16_t w, uint16_t h, draw_c
         h += y;
         y = 0;
     }
-    uint64_t offset = x + y * ctx->pitch;
+    uint64_t offset = x * sizeof(draw_color_t) + y * ctx->pitch;
     for(int yy = 0; yy < h && y + yy < ctx->height; yy++) {
         for(int xx = 0; xx < w && x + xx < ctx->width; xx++) {
-            putpixel(ctx, offset + xx, color);
+            putpixel(ctx, offset + xx * sizeof(draw_color_t), color);
         }
         offset += ctx->pitch;
     }
