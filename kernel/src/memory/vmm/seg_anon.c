88ba2aa0ad9709b8a3b94f21cc7e9b6096f1d6df
WuX <wux@thenest.dev>
2024-04-16 11:19:34 +0300

    fix: critical bug, dont know how I havent caught this before

--- kernel/src/memory/vmm/seg_anon.c
+++ kernel/src/memory/vmm/seg_anon.c
@@ -10,7 +10,7 @@ static void map(vmm_segment_t *segment, uintptr_t address) {
     int pmm_flags = PMM_STANDARD;
     int vmm_flags = ARCH_VMM_FLAG_NONE;
     if(segment->address_space != g_vmm_kernel_address_space) vmm_flags |= ARCH_VMM_FLAG_USER;
-    if(HAS_FLAG(segment, SEG_ANON_FLAG_ZERO)) vmm_flags |= PMM_FLAG_ZERO;
+    if(HAS_FLAG(segment, SEG_ANON_FLAG_ZERO)) pmm_flags |= PMM_FLAG_ZERO;
     pmm_page_t *page = pmm_alloc_page(pmm_flags);
     arch_vmm_map(segment->address_space, address, page->paddr, segment->protection, vmm_flags);
 }
