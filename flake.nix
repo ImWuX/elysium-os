367caa738424342752307966933d4a911ca771f6
WuX <wux@thenest.dev>
2024-10-25 15:42:48 +0300

    tool: add nix shell

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { nixpkgs, flake-utils, ... } @ inputs: flake-utils.lib.eachDefaultSystem (system:
+        let
+            pkgs = import nixpkgs { inherit system; };
+            inherit (pkgs) lib stdenv mkShell fetchFromGitLab buildGoModule;
+        in {
+            devShells.default = mkShell {
+                shellHook = "export DEVSHELL_PS1_PREFIX='elysium-os'; export PATH=\"$PATH:../chariot\";";
+                nativeBuildInputs = with pkgs; [
+                    gnumake
+                    gdb
+                    gcc13
+                    qemu_full
+                ];
+            };
+        }
+    );
+}
